0c0e1fc863cf52fbfc6412daa26ad30a940f144d
Bjverde <bjverde@yahoo.com.br>
2026-09-13 02:28:33 -0300

    :hammer: #33 alterando application.php

--- framework/app/config/application.php
+++ framework/app/config/application.php
@@ -4,8 +4,8 @@ return [
         'timezone' => 'America/Sao_Paulo',
         'language' => 'pt',
         'application' => 'sample',
-        'title' => 'Adianti Framework 8.6',
-        'theme' => 'adminbs5',
+        'title' => 'Adianti Fork Framework',
+        'theme' => 'adminbs5_v7',
         'debug' => '1',
         'strict_request' => '0'
     ],
@@ -21,5 +21,11 @@ return [
             'menu_mode' => 'dark',
             'main_mode' => 'light'
         ]
-    ]
+    ],
+    'system' =>  [
+        'system_version' => '8.6.0',
+        'system_name_sub' => 'Fork do Adianti FrameWork',
+        'adianti_min_version' => '8.6.0',
+        'formdin_min_version' => '5.9.0',
+    ],
 ];
