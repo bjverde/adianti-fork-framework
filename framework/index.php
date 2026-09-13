43050143eff63eab114fd91c09e6cd637f9c39f5
Bjverde <bjverde@yahoo.com.br>
2026-09-13 02:35:02 -0300

    🔨 #33 alterando index.php

--- framework/index.php
+++ framework/index.php
@@ -5,17 +5,25 @@ $ini = AdiantiApplicationConfig::get();
 $theme  = $ini['general']['theme'];
 new TSession;
 
-if (isset($_REQUEST['template']) AND $_REQUEST['template'] == 'iframe')
-{
+if (isset($_REQUEST['template']) AND $_REQUEST['template'] == 'iframe') {
 	$content = file_get_contents("app/templates/{$theme}/iframe.html");
-}
-else
-{
+} else {
 	$content  = file_get_contents("app/templates/{$theme}/layout.html");
 }
 
 $menu_string = AdiantiMenuBuilder::parse('menu.xml', $theme);
 $content     = ApplicationTranslator::translateTemplate($content);
+
+//--- START: TEMA ADMINBS5_V7  ---------------------------------------------------------
+$system_version = $ini['system']['system_version'];
+$title          = $ini['general']['title'];
+$head_title     = $title.' - v'.$system_version;
+
+$content = str_replace('{head_title}', $head_title, $content);
+$content = str_replace('{title}', $title, $content);
+$content = str_replace('{system_version}', $system_version, $content);
+//--- END: TEMA ADMINBS5_V7 ------------------------------------------------------------
+
 $content     = str_replace('{LIBRARIES}', file_get_contents("app/templates/{$theme}/libraries.html"), $content);
 $content     = str_replace('{class}', isset($_REQUEST['class']) ? $_REQUEST['class'] : '', $content);
 $content     = str_replace('{template}', $theme, $content);
